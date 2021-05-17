dc1ee59925388834d549727adfefe45cc9b9338d
sandeep081 <67308644+sandeep081@users.noreply.github.com>
2021-05-17 15:18:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,18 +37,6 @@ def predict_note_authentication(chanel, region, fresh, milk, grocery, frozen, de
 
 
 def main():
-    html_temp = """
-   <div class="" style="background-color:blue;" >
-   <div class="clearfix">           
-   <div class="col-md-12">
-   <center><p style="font-size:40px;color:white;margin-top:10px;">Poornima Institute of Engineering & Technology</p></center> 
-   <center><p style="font-size:30px;color:white;margin-top:10px;">Department of Computer Engineering</p></center> 
-   <center><p style="font-size:25px;color:white;margin-top:10px;"Machine Learning Lab Experiment</p></center> 
-   </div>
-   </div>
-   </div>
-   """
-    st.markdown(html_temp, unsafe_allow_html=True)
     st.header("Customer Segmenation on wholesale data ")
 
     chanel = st.selectbox(
